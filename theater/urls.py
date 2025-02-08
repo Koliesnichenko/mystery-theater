12186da8d1c1129dbd5562851cfb8e0ce464ca74
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-02-08 14:24:35 +0200

    add API Urls and routers

---
+++ theater/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from theater.views import (
+    GenreViewSet,
+    ActorViewSet,
+    PlayViewSet,
+    PerformanceViewSet,
+    TicketViewSet,
+    TheaterHallViewSet,
+    ReservationViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("plays", PlayViewSet)
+router.register("performances", PerformanceViewSet)
+router.register("tickets", TicketViewSet)
+router.register("theater_halls", TheaterHallViewSet)
+router.register("reservations", ReservationViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "theater"
