72db85a72d1d344f7859218ba22adefe0134491d
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-02-10 19:46:48 +0200

    add ability to book tickets and reservations via admin panel

--- theater/admin.py
+++ theater/admin.py
@@ -10,10 +10,20 @@ from theater.models import (
     TheaterHall,
 )
 
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
 admin.site.register(Genre)
 admin.site.register(Actor)
 admin.site.register(Play)
 admin.site.register(Ticket)
 admin.site.register(Performance)
 admin.site.register(TheaterHall)
-admin.site.register(Reservation)
